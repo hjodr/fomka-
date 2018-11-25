9916bcf3aa6ca48f0a9c1b62cd2f66e6b53d47a6
Богдан Брижатый <demaskinas@yandex.ua>
2018-11-25 20:58:57 +0200

    fix for age

--- Fomka-Web-App/Seed.sql
+++ Fomka-Web-App/Seed.sql
@@ -27,10 +27,10 @@ set identity_insert [User] on
 
 insert into [User] (Id, FirstName, LastName, SurName, [Group], Email, DateOfBirth, [Login], [Password], [Type])
 values
-	(1, 'Bohdan', 'Bryzhatyi', '', 1, 'email', dateadd(Y, -12, getutcdate()), 'bbrizhaty', 'bbrizhaty', 1),
-	(2, 'Dmutro', 'Tsubera', '', 1, 'email', dateadd(Y, -12, getutcdate()), 'dtsubera', 'dtsubera', 1),
-	(3, 'Olena', 'Niemova', '', 1, 'email', dateadd(Y, -12, getutcdate()), 'oniemova', 'oniemova', 1),
-	(4, 'Ivan', 'Kuts', '', 1, 'email', dateadd(Y, -12, getutcdate()), 'ikuts', 'ikuts', 1),
-	(5, 'Lecturer', 'Demo', '', 1, 'email', dateadd(Y, -12, getutcdate()), 'lecturerdemo', 'lecturerdemo', 2)
+	(1, 'Bohdan', 'Bryzhatyi', '', 1, 'email', dateadd(Y, -21, getutcdate()), 'bbrizhaty', 'bbrizhaty', 1),
+	(2, 'Dmutro', 'Tsubera', '', 1, 'email', dateadd(Y, -21, getutcdate()), 'dtsubera', 'dtsubera', 1),
+	(3, 'Olena', 'Niemova', '', 1, 'email', dateadd(Y, -21, getutcdate()), 'oniemova', 'oniemova', 1),
+	(4, 'Ivan', 'Kuts', '', 1, 'email', dateadd(Y, -21, getutcdate()), 'ikuts', 'ikuts', 1),
+	(5, 'Lecturer', 'Demo', '', 1, 'email', dateadd(Y, -21, getutcdate()), 'lecturerdemo', 'lecturerdemo', 2)
 
 set identity_insert [User] off
