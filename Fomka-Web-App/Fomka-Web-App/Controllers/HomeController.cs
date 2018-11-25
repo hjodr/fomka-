e75bb7ecc39528f97e6a9ceb630cfcb227fb8aac
Dmytro Tsubera <dtsubera@gmail.com>
2018-11-25 16:42:44 +0200

    Revert "Revert "efcore""
    This reverts commit d06f35111dd6c0426b1bbe83ecd48fd402c5dd61.

--- Fomka-Web-App/Fomka-Web-App/Controllers/HomeController.cs
+++ Fomka-Web-App/Fomka-Web-App/Controllers/HomeController.cs
@@ -15,13 +15,6 @@ namespace Fomka_Web_App.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
         public IActionResult Privacy()
         {
             return View();
