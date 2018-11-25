5aa960eee898ac80620a8482f8e61de473f9838d
Богдан Брижатый <demaskinas@yandex.ua>
2018-11-25 20:49:01 +0200

    data seed for users

--- Fomka-Web-App/Seed.sql
+++ Fomka-Web-App/Seed.sql
@@ -7,3 +7,30 @@ set identity_insert UserType on
 insert into UserType (Id, Title) values (1, 'Student'), (2, 'Lecturer');
 
 set identity_insert UserType off
+
+GO
+set identity_insert [Institute] on
+insert into [Institute] (Id, Title)
+	values (1, 'ШКНІ');
+set identity_insert [Institute] off
+
+
+GO
+set identity_insert [Group] on
+insert into [Group] (Id, Title, InstituteId)
+	values (1, 'ПЗ-35', 1);
+set identity_insert [Group] off
+
+
+GO
+set identity_insert [User] on
+
+insert into [User] (Id, FirstName, LastName, SurName, [Group], Email, DateOfBirth, [Login], [Password], [Type])
+values
+	(1, 'Bohdan', 'Bryzhatyi', '', 1, 'email', dateadd(Y, -12, getutcdate()), 'bbrizhaty', 'bbrizhaty', 1),
+	(2, 'Dmutro', 'Tsubera', '', 1, 'email', dateadd(Y, -12, getutcdate()), 'dtsubera', 'dtsubera', 1),
+	(3, 'Olena', 'Niemova', '', 1, 'email', dateadd(Y, -12, getutcdate()), 'oniemova', 'oniemova', 1),
+	(4, 'Ivan', 'Kuts', '', 1, 'email', dateadd(Y, -12, getutcdate()), 'ikuts', 'ikuts', 1),
+	(5, 'Lecturer', 'Demo', '', 1, 'email', dateadd(Y, -12, getutcdate()), 'lecturerdemo', 'lecturerdemo', 2)
+
+set identity_insert [User] off
