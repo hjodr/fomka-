61bfcce1dc9f0614ef84263491967c79637ae93a
Куц Иван Игоревич <ivan050005@gmail.com>
2018-11-25 19:39:44 +0200

    Generate task

---
+++ Fomka-Web-App/fomka-web/Models/BlockOfCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace fomka_web.Models
+{
+    public class BlockOfCode
+    {
+        public int BlockID { get; set; }
+
+        public string Code { get; set; }
+    }
+}
